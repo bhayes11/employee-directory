d68eacfb7b172a80b8f6aa00f6bab58389bfe7f0
Hayes11 <bhayes81@gmail.com>
2021-01-23 17:29:24 -0500

    added components and containers folders/files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-// import React from "react";
+import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "./containers/Home/Home";
+import Search from "./components/Search/Search"; 
 
 function App() {
   return (
     <Router basename="/">
-      <Route exact path="/" component={Home} />
-      <Route exact path="/home" component={Home} />
+      <Search />
+      <Home />
     </Router>
   );
 }
