288de2660d0e815bcd6ec18c26a3b7192232d759
Hayes11 <bhayes81@gmail.com>
2021-01-23 21:12:46 -0500

    changed class to className

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Heading = () => {
     return (
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">Employee Directory</h1>
-                <p class="lead">Click on carrots to filter by heading or use the search box to narrow your results.</p>
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Employee Directory</h1>
+                <p className="lead">Click on carrots to filter by heading or use the search box to narrow your results.</p>
             </div>
         </div>
     );
--- utils/API.js
+++ utils/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import API from "../../utils/API";
 // Export an object containing methods we'll use for accessing the randomuser API
 
 export default {
