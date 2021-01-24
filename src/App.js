97b90f44cc98c8470d17067644bca50f00775f4f
Hayes11 <bhayes81@gmail.com>
2021-01-23 20:17:42 -0500

    changed app.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router} from "react-router-dom";
+// import { HashRouter as Router, Route} from "react-router-dom";
 import Home from "./containers/Home/Home";
 import Search from "./components/Search/Search"; 
 import Heading from "./components/Heading/Heading"; 
@@ -9,8 +10,7 @@ function App() {
     <Router basename="/">
       <Heading />
       <Search />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/home" component={Home} />
+      <Home />
     </Router>
   );
 }
